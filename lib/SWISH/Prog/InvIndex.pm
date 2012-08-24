9c98d469892f2d1882a06b9625d3d8e03b9c4849
Peter Karman <perl@peknet.com>
2012-08-24 03:12:49 +0000

    Import of KARMAN/SWISH-Prog-0.61 from CPAN.
    gitpan-cpan-distribution: SWISH-Prog
    gitpan-cpan-version:      0.61
    gitpan-cpan-path:         KARMAN/SWISH-Prog-0.61.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

--- lib/SWISH/Prog/InvIndex.pm
+++ lib/SWISH/Prog/InvIndex.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use base qw( SWISH::Prog::Class );
 use Carp;
-use Path::Class ();   # do not import file() and dir()
+use Path::Class ();    # do not import file() and dir()
 use Scalar::Util qw( blessed );
 use SWISH::Prog::InvIndex::Meta;
 use overload(
@@ -13,7 +13,7 @@ use overload(
     fallback => 1,
 );
 
-our $VERSION = '0.60';
+our $VERSION = '0.61';
 
 __PACKAGE__->mk_accessors(qw( path clobber ));
 
